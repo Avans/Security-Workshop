23d5f19e39f099364a2ea69747e855b4f1ffed01
Paul Wagener <mail@paulwagener.nl>
2013-12-09 02:35:14 -0800

    Use separate configuration files for vmbuilder

--- build-vm/build.sh
+++ build-vm/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sed "s|BUILDDIR|$BUILDDIR|g" < vmbuilder.cfg.template  > vmbuilder.cfg 
+sed "s|BUILDDIR|$BUILDDIR|g" < vmbuilder.copy.template > vmbuilder.copy
 
 echo "Building Virtual Machine for Avans Security Workshop..."
 echo "(Requires vmbuilder, install with: 'sudo apt-get install python-vm-builder')"
 
-sudo vmbuilder vmw6 ubuntu --flavour virtual --arch i386 -o \
-  --firstboot $DIR/firstboot.sh \
-  --user security --pass security --name "Security Workshop" \
-  --addpkg apache2 --addpkg apache2-mpm-prefork \
-  --addpkg git-core \
-  --addpkg php5-cli --addpkg php5-mysql --addpkg libapache2-mod-php5 \
-  --addpkg mysql-server --addpkg mysql-client
+echo sudo vmbuilder vmw6 ubuntu --config vmbuilder.cfg -o
