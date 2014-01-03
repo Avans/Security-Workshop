68a62c851a8b238ebed502420d0d1563f3e076b3
Paul Wagener <mail@paulwagener.nl>
2014-01-03 05:39:32 -0800

    Added phantomjs to VM

--- build-vm/build.sh
+++ build-vm/build.sh
@@ -2,10 +2,10 @@
 
 BUILDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sed "s|BUILDDIR|$BUILDDIR|g" < vmbuilder.cfg.template  > vmbuilder.cfg 
+sed "s|BUILDDIR|$BUILDDIR|g" < vmbuilder.cfg.template  > vmbuilder.cfg
 sed "s|BUILDDIR|$BUILDDIR|g" < vmbuilder.copy.template > vmbuilder.copy
 
 echo "Building Virtual Machine for Avans Security Workshop..."
 echo "(Requires vmbuilder, install with: 'sudo apt-get install python-vm-builder')"
 
-echo sudo vmbuilder vmw6 ubuntu --config vmbuilder.cfg -o
+sudo vmbuilder vmw6 ubuntu --config vmbuilder.cfg -o
