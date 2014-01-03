9828a079e0ee5ba8a813c1b1256640d36ddb9c52
Paul Wagener <mail@paulwagener.nl>
2014-01-03 14:36:29 +0000

    Automatically configure Apache

--- build-vm/boot.sh
+++ build-vm/boot.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+# This script runs each time the VM starts up
+
 echo
 cd /home/security/workshop
+
+# Update repository
 git pull origin master
 
+# Update Apache configuration
+cp build-vm/security-workshop.apache.conf /etc/apache2/sites-available/security-workshop.conf
+a2dissite 000-default
+a2ensite security-workshop
+service apache2 reload
+
+# Show welcome message
 IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \$1}'`
 
 echo
