8ef7b0ba1a639c1e890febfaae6b33f3e22a6e2b
Paul Wagener <mail@paulwagener.nl>
2014-01-03 14:00:26 +0000

    Show IP on VM startup

--- build-vm/boot.sh
+++ build-vm/boot.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-echo "BOOOOOOOT!"
-echo "boot..."
+IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \$1}'`
+
+echo
+echo
+echo " De Security Workshop VM is nu opgestart"
+echo " Ga met je browser naar deze URL:"
+echo
+echo "       \033[0;32mhttp://$IP/\033[0m"
+echo
