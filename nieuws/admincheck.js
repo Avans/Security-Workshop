07a54d26928a354eeb344cf08b7287e703990d70
Paul Wagener <mail@paulwagener.nl>
2014-03-19 10:01:10 +0100

    Fixed admincheck.js

--- nieuws/admincheck.js
+++ nieuws/admincheck.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.open('http://localhost/nieuws/', 'post', 'gebruikersnaam=Admin&wachtwoord=sesame', function() {
+page.open('http://localhost/nieuws/login.php', 'post', 'gebruikersnaam=Admin&wachtwoord=sesame', function() {
     setTimeout(function(){
         phantom.exit();
     }, 1000);
