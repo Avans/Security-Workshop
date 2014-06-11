8da91538e233d42807497006d36d52920e31a554
Paul Wagener <mail@paulwagener.nl>
2014-06-11 14:29:03 +0200

    Added file useful for grading

---
+++ bank/checkresetbob.php
@@ -0,0 +1,17 @@
+<?php
+$connection = new mysqli('localhost', 'bank', 'pass', 'bank');
+$result = $connection->query("SELECT * FROM gebruikers WHERE gebruikersnaam = 'Bob'");
+$row = $result->fetch_array();
+echo "Bob heeft: " . $row['balans'] . "<br>";
+
+if ((int)$row['balans'] >= 1000000) {
+    echo "Bob is miljonair<br>";
+}
+
+echo "Updating back to 10.00<br>";
+
+$connection->query("UPDATE gebruikers SET balans = 10.00 WHERE gebruikersnaam = 'Bob'");
+$connection->close();
+
+
+?>
