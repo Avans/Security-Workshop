f64bab2c079de6f07fe736753465623e505acce7
Paul Wagener <mail@paulwagener.nl>
2015-03-09 13:30:02 +0100

    Removed headers

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 De virtuele machine werkt! Lees de opgave om te beginnen met hacken.
 
-<h2><a href="/bank/">Bank</a></h2>
+<p>
+<a href="/bank/">Bank</a><br />
 <a href="/bank/index_multi.php">Bank (multi_query)</a><br />
 <a href="/bank/index_xss.php">Bank (xss)</a>
+</p>
 
-<h2><a href="/webshop/">Webshop</a></h2>
+<p>
+<a href="/webshop/">Webshop</a><br>
 <a href="/webshop/product_detail_replace.php?id=1">Webshop (replace)</a>
+</p>
 
-<h2><a href="/wereldwijs/index.php?id=1">Wereldwijs</a></h2>
+<p><a href="/wereldwijs/index.php?id=1">Wereldwijs</a></p>
 
-<h2><a href="/nieuws/">Nieuws</a></h2>
+<p>
+<a href="/nieuws/">Nieuws</a><br>
 <a href="/nieuws/users.php">Nieuws (users)</a><br>
 <a href="/nieuws/login.php">Nieuws (login)</a>
+</p>
 
-<h2><a href="/image/">Image</a></h2>
+<p>
+<a href="/image/">Image</a><br>
 <a href="/image/index_no_public_uploads.php">Image 2</a><br>
 <a href="/image/index_more_pages.php">Image 3</a>
+</p>
 
 <hr>
 <footer>Laatste update: <?php echo `git log -1 --format="%cd"`; ?></footer>
